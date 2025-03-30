aeff24a98e57097fe08d7089fc5762267f6c03a4
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 15:28:26 -0300

    8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,7 +3,7 @@
 // Constructor attributes:
 // size (Number)
 // location (String)
-// Each attribute must be stored in an “underscore” attribute version 
+// Each attribute must be stored in an “underscore” attribute version
 // (ex: name is stored in _name)
 // When the class is cast into a Number, it should return the size.
 // When the class is cast into a String, it should return the location.
