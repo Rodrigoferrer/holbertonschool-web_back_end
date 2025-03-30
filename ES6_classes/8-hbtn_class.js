57374801a356e27aef7d56a0c70e37cd95a0a1bd
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 15:27:53 -0300

    8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,27 +3,25 @@
 // Constructor attributes:
 // size (Number)
 // location (String)
-// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Each attribute must be stored in an “underscore” attribute version 
+// (ex: name is stored in _name)
 // When the class is cast into a Number, it should return the size.
 // When the class is cast into a String, it should return the location.
 // salida
 
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number') throw new TypeError("Size must be a number")
-    if (typeof location !== 'string') throw new TypeError("location must be a string")
-    this._size = size
-    this._location = location
+    if (typeof size !== 'number') throw new TypeError('Size must be a number');
+    if (typeof location !== 'string') throw new TypeError('location must be a string');
+    this._size = size;
+    this._location = location;
   }
 
-  valueOf() 
-  {
-    return this._size  
+  valueOf() {
+    return this._size;
   }
-  
-  toString() 
-  {
-    return this._location 
+
+  toString() {
+    return this._location;
   }
-  
 }
