e84d34bce4317baa9fe133ae6d71ed9ff51d0faa
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 14:14:04 -0300

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,36 @@
-//Implement a class named Currency:
+// Implement a class named Currency:
 // - Constructor attributes:
 // code (String)
 // name (String)
-// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Each attribute must be stored in an “underscore” attribute version
+//  (ex: name is stored in _name)
 // Implement a getter and setter for each attribute.
-// Implement a method named displayFullCurrency that will return the attributes in the following format name (code).
+// Implement a method named displayFullCurrency
+//  that will return the attributes in the following format name (code).
 
-export default class Currency{
-  constructor(code, name){
-    this._code = code
-    this._name = name
-  }
-  get name(){
-    return this._name
-  }
-  set name(value){
-    this._name = value
-  }
-  get code(){
-    return this._code
-  }
-  set code(value){
-    this._code = value
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
 
- get displayFullCurrency(){
-  return this._code
- }
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
