a1ac6524adf61acdf12415260ca8d88d585c71bb
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 13:12:00 -0300

    1-make_classrooms.js corregido

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/extensions */
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }
 
