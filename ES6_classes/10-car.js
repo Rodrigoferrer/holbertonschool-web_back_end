9caf7ea28aa7182c80d675e9f263fa5a66425d83
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 15:48:07 -0300

    10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -7,19 +7,20 @@
 // Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
 // Add a method named cloneCar. This method should return a new object of the class.
 
+const cloneSymbol = Symbol('clone');
+
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw new TypeError('Subject must be a string');
-    if (typeof motor !== 'string') throw new TypeError('Subject must be a string');
-    if (typeof color !== 'string') throw new TypeError('Subject must be a string');
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
+  [cloneSymbol]() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
   cloneCar() {
-    const newObject = new this.constructor(this._brand, this._motor, this._color);
-    return newObject;
+    return this[cloneSymbol]();
   }
 }
