7005e531d1e851014bbadb082310bb3089b6805d
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-30 15:03:27 -0300

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,14 @@
-// Implement a class named Building:
-// Constructor attributes:
-// sqft (Number)
-// Each attribute must be stored in an
-// “underscore” attribute version (ex: name is stored in _name)
-// Implement a getter for each attribute.
-// Consider this class as an abstract class.
-// And make sure that any class that extends from it should
-// implement a method named evacuationWarningMessage.
-// If a class that extends from it does not have a evacuationWarningMessage method,
-// throw an error with the message Class extending Building must override evacuationWarningMessage
-
-export default class Building {
+class Building {
   constructor(sqft) {
     if (new.target === Building) {
       throw new Error('Cannot instantiate abstract class Building');
     }
+
+    // Verificar que la clase hija ha implementado evacuationWarningMessage
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
@@ -22,7 +16,9 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
+
+export default Building;
