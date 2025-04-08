5fee06d9ebb6b5d3a4ab9e4240dc686570cd7c51
Rodrigo <rodrigoferrer30@gmail.com>
2025-04-08 15:39:51 -0300

    task 2

---
+++ ES6_data_manipulation/programa.py
@@ -0,0 +1,37 @@
+# Se te daPalabras. Algunas palabras pueden repetirse. Para cada palabra, muestre el número de ocurrencias. 
+# El orden de salida debe coincidir con el orden de entrada de la palabra. Consulte el ejemplo de entrada/salida para obtener más información.
+
+# Nota: Cada línea de entrada termina con un carácter "\n" .
+
+# Restricciones:
+
+# La suma de las longitudes de todas las palabras no excede
+# Todas las palabras están compuestas únicamente de letras minúsculas del idioma inglés.
+
+# Formato de entrada
+
+# La primera línea contiene el número entero,.
+# El siguienteCada línea contiene una palabra.
+
+# Formato de salida
+
+# ProducciónLíneas.
+# En la primera línea, se muestra el número de palabras distintas de la entrada.
+# En la segunda línea, se muestra el número de ocurrencias de cada palabra distinta según su aparición en la entrada.
+
+# Entrada de muestra
+
+# 4
+# bcdef
+# abcdefg
+# bcde
+# bcdef
+# Salida de muestra
+
+# 3
+# 2 1 1
+# Explicación
+
+# HayPalabras distintas. Aquí, "bcdef" aparece dos veces en la entrada, 
+# en la primera y la última posición. Las demás palabras aparecen una vez cada una. 
+# El orden de las primeras apariciones es "bcdef" , "abcdefg" y "bcde" , que corresponde a la salida.
