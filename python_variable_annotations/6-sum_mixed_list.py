4868589deb64c47c7afcda8c0eb70ce537b4acb4
Rodrigo <rodrigoferrer30@gmail.com>
2025-04-27 22:01:22 -0300

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module python3"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of a list of varios types, using union to fuze
+    floats and integers"""
+    return sum(mxd_lst)
