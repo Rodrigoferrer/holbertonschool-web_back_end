edda0b3e5cf1b9e3763c52817e347cebd702d5f0
Rodrigo <rodrigoferrer30@gmail.com>
2025-04-27 21:55:06 -0300

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module python3"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Type anotated fn that
+    returns the floor of a float
+    """
+    return math.floor(n)
