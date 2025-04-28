c3c49f28eeb3dac138452cca532ad52a272f8e0e
Rodrigo <rodrigoferrer30@gmail.com>
2025-04-27 21:56:26 -0300

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module python3"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type anotated fn that
+    returns the string representation of a float
+    """
+    return str(n)
