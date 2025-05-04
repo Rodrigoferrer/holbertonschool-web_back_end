5aca82856367d3c0bf37c8f5d9abbe0acfea75f7
Rodrigo <rodrigoferrer30@gmail.com>
2025-05-04 20:03:04 -0300

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,8 +35,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get a page of the dataset."""
 
-        assert isinstance(page, int) and page > 0, "page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
