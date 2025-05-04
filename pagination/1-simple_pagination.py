a9fec9a0c334637b7f3f284a53c8335f3b95a66c
Rodrigo <rodrigoferrer30@gmail.com>
2025-05-04 20:01:20 -0300

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -14,19 +14,22 @@ def index_range(page: int, page_size: int):
     end_index = start_index + page_size
     return (start_index, end_index)
 
+
 class Server:
     """Server class to paginate a database of popular baby names."""
-    
     DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         self.__dataset = None
+
     def dataset(self) -> List[List]:
         """Cached dataset."""
+
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]  # Skip header row
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
@@ -36,3 +39,8 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
 
         dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
