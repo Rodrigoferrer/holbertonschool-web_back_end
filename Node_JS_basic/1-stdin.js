0cf1ef35fcf66fbc25b21038613d8f4e554ad957
Rodrigo <rodrigoferrer30@gmail.com>
2025-05-25 21:41:15 -0300

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-/*
-Nodejs Module
-*/
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
 
 console.log("Welcome to Holberton School, what is your name?");
 
